d36d0846a575b6ea666c5075916e59495a82d907
Goward_Silk <megaupfunguj@gmail.com>
2025-06-06 14:06:15 +0200

    Setting up parsing of basic headers (cl_platform.h)

---
+++ CL/main.py
@@ -0,0 +1,59 @@
+import sys
+import argparse
+import os
+import shutil
+import parsegen
+
+def print_help() -> None:
+    print("py main.py [--parsegen] [--out=<outdir>] [--cc=<cc>]")
+    print("\t--parsegen ... generates odin files out of cl headers (set by default)")
+    print("\t--out      ... specifies output directory for *.odin bindings")
+    print("\t--cc       ... specifies C compiler to be used for CL header preprocessing")
+
+def arg_validator(s: str) -> str:
+    s = s.lower()
+
+    match s:
+        case "clang" | "gcc" | "cl":
+            return s
+        case _:
+            raise argparse.ArgumentTypeError("Invalid value for --cc (expected: clang|gcc|cl).")
+
+def find_cc(cc: str | None) -> str:
+    if not cc:
+        compilers = ["clang", "gcc", "cl"]
+        cc = next((c for c in compilers if shutil.which(c)), None)
+    else:
+        # cc has already been validated and normalized by arg_validator
+        if not shutil.which(cc):
+            print(f"\x1b[31mCompiler '{cc}' not found in PATH.\x1b[0m")
+            exit(1)
+
+    if not cc:
+        print("\x1b[31mNo working compiler found in PATH.\x1b[0m")
+        exit(1)
+
+    return shutil.which(cc)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--parsegen", action="store_true", help="Generates odin files out of cl headers")
+    parser.add_argument("--out", type=str, help="Specifies output directory of *.odin bindings")
+    parser.add_argument("--cc", type=arg_validator, help="Specifies C compiler to be used for CL header preprocessing")
+    parser.add_argument("--help", action="store_true", help="Show help message")
+
+    args, unknown = parser.parse_known_args()
+
+    if unknown:
+        print(f"\x1b[31mInvalid\x1b[0m parameter(s): {' '.join(unknown)}")
+        print_help()
+        sys.exit(1)
+    elif args.help:
+        print_help()
+    else:
+        out_dir = args.out or os.getcwd()
+        parsegen.main(find_cc(args.cc), out_dir)
+
+if __name__ == "__main__":
+    main()
---
+++ CL/parsegen.py
@@ -0,0 +1,478 @@
+import os
+import re
+import subprocess
+from dataclasses import dataclass
+from typing import Optional
+from enum import Enum
+
+Type = str
+
+# class Type:
+#     ctype: str # string of c type code (should be used to indetify for c to odin conversion if necessary)
+#     otype: str # odin type (transformed from ctype during parsing)
+
+class Definition:
+    def into_odin(self) -> str:
+        return f"{self.key} :: {self.value}"
+
+    key:    str
+    value:  str
+
+class Attribute(Enum):
+    NONE    = 0
+    STDCALL = 1
+
+class Function:
+    def into_odin(self) -> str:
+        if ret == "void":
+            params_str: str = ""
+            for index, param in params:
+                params_str += f"{params[1]}: {params[0]}{", " if index < params.len() - 1 else ""}"
+
+            return f"{name} :: proc {"\"stdcall\"" if attr == Attribute.STDCALL else ""} ({params_str}) ---"
+
+        return f"{name} :: proc {"\"stdcall\"" if attr == Attribute.STDCALL else ""} ({params_str}) -> {ret} ---"
+
+    attr:   Attribute
+    ret:    Type
+    params: list[tuple[Type, str]]
+    name:   str
+
+class Alias:
+    _from:  str
+    _to:    str
+
+g_definitions: list[Definition] = []
+g_aliases: dict[str, str] = {}
+g_functions: list[Function] = []
+
+@dataclass
+class CTypeMapping:
+    def __getitem__(self, index) -> Optional[str]:
+        assert index == 0 or index == 1
+        return self.signed if index == 0 else self.unsigned
+
+    signed: Optional[str]
+    unsigned: Optional[str]
+
+c_to_odin_types = {
+    "__int8":      CTypeMapping("i8",   "u8"),
+    "__int16":     CTypeMapping("i16",  "u16"),
+    "__int32":     CTypeMapping("i32",  "u32"),
+    "__int64":     CTypeMapping("i64",  "u64"),
+
+    # CLANG/MSVC ONLY
+    "__m128":      CTypeMapping("#simd[4]f32", ""),
+    "__m128d":     CTypeMapping("#simd[2]f64", ""),
+
+    "__m128i":     CTypeMapping("#simd[4]i32", ""),
+    "__m64":       CTypeMapping("#simd[2]i32", ""),
+
+    "int8_t":      CTypeMapping("i8",   "u8"),
+    "uint8_t":     CTypeMapping(None,  "u8"),
+    "int16_t":     CTypeMapping("i16",  "u16"),
+    "uint16_t":    CTypeMapping(None,  "u16"),
+    "int32_t":     CTypeMapping("i32",  "u32"),
+    "uint32_t":    CTypeMapping(None,  "u32"),
+    "int64_t":     CTypeMapping("i64",  "u64"),
+    "uint64_t":    CTypeMapping(None,  "u64"),
+
+    "char":        CTypeMapping("i8",   "u8"),
+    "short":       CTypeMapping("i16",  "u16"),
+    "int":         CTypeMapping("i32",  "u32"),
+    "long":        CTypeMapping("i64",  "u64"),
+
+    "float":       CTypeMapping("f32",  None),
+    "double":      CTypeMapping("f64",  None),
+
+    "size_t":      CTypeMapping("uintptr", None),
+    "intptr_t":    CTypeMapping("intptr", None),
+    "uintptr_t":   CTypeMapping("uintptr", None),
+
+    # does this have to be here?
+    "void*":       CTypeMapping("rawptr", None),
+}
+
+"""
+Extracts all lines belonging to `target_file' after running preprocessor (preprocessor
+    output absolute file path expected in `preprocessed_fname') in one string.
+"""
+def extract_file_content(preprocessed_fname: str, target_file: str) -> str:
+    current_file: str | None = None
+    output_lines: list[str] = []
+
+    line_marker_re = re.compile(r'^#\s+(\d+)\s+"([^"]+)"(?:\s+(\d+))?')
+
+    preprocessed_text: str = ""
+    with open(preprocessed_fname, "r") as file:
+        preprocessed_text = file.read()
+
+    for line in preprocessed_text.splitlines():
+        match = line_marker_re.match(line)
+        if match:
+            _, file_name, *_ = match.groups() # original_line_number, file_name, flags
+            current_file = file_name.replace("\\", "/")
+        elif current_file and target_file in current_file and line:
+            output_lines.append(line)
+
+    return "\n".join(output_lines)
+
+def is_delim(s: chr) -> bool:
+    match s[0]:
+        case ',' | ';' | '*' | '{' | '}' | '[' | ']' | '(' | ')':
+            return True
+        case _:
+            return False
+
+def next_token(s: str, start: int) -> tuple[str, int] | None:
+    # skip isspace
+    while start < len(s) and s[start].isspace():
+        #print(f"\r\x1b[34mSkipping: {s[start]}\x1b[0m")
+        start += 1
+    if start >= len(s):
+        return None
+
+    # handle single chars first
+    if is_delim(s[start]):
+        #print(f"\r\x1b[34mFound: {s[start]}\x1b[0m")
+        return s[start], start + 1
+
+    end = start
+    while end < len(s) and not (s[end].isspace() or is_delim(s[end])):
+        #print(f"\x1b[32mReading: {s[end]}\x1b[0m")
+        end += 1
+
+    return s[start:end], end
+
+def next_token_unwrap(fcontent, cursor) -> tuple[str, int]:
+    result = next_token(fcontent, cursor)
+    assert result
+    return result
+
+def parse_member_type_array_identifier(fcontent, cursor) -> tuple[str, int]:
+    number, next_cursor = next_token_unwrap(fcontent, cursor)
+    if number[0].isdigit():
+        _, next_cursor = next_token_unwrap(fcontent, next_cursor)
+        print(f"Returning [{number}]")
+        return f"[{number}]", next_cursor
+    return f"[]", next_cursor
+
+def parse_member_type(fcontent, cursor) -> tuple[str, int]:
+    _type, cursor = parse_type(fcontent, cursor)
+    # parse name/array identifier or both
+    name, next_cursor = next_token_unwrap(fcontent, cursor)
+
+    # end
+    if is_delim(name):
+        #print(f"Previous: {next_token_unwrap(fcontent, cursor)}")
+        #print(f"Current: {next_token_unwrap(fcontent, next_cursor)}")
+        return f"using: _: {_type},\n", next_cursor
+
+    # name
+    cursor = next_cursor
+    next, next_cursor = next_token_unwrap(fcontent, cursor)
+    if next == "[":
+        arr, next_cursor = parse_member_type_array_identifier(fcontent, next_cursor);
+        _, next_cursor = next_token_unwrap(fcontent, next_cursor)
+        return f"{name}: {arr}{_type},\n", next_cursor
+    elif next == ",":
+        next_cursor = cursor # rewind 1 token back
+        while True:
+            next, next_cursor = next_token_unwrap(fcontent, next_cursor);
+            if next == ";":
+                break
+            name += next # grab ','
+            next, next_cursor = next_token_unwrap(fcontent, next_cursor);
+            name += next # grab next name
+        cursor = next_cursor
+        print(f"Returning {name}: {_type},\n")
+        return f"{name}: {_type},\n", cursor
+    else:
+        _, cursor = next_token_unwrap(fcontent, cursor)
+        return f"{name}: {_type},\n", cursor
+
+def parse_compound_type(fcontent, cursor) -> tuple[str, int]:
+    # parse types until "}"
+    blob: str = ""
+    while True:
+        word, next_cursor = next_token_unwrap(fcontent, cursor)
+        if word == "}":
+            # word, _ = next_token_unwrap(fcontent, next_cursor)
+            # if word == ";":
+            #     cursor = next_cursor
+            break
+
+        word, cursor = parse_member_type(fcontent, cursor)
+        blob += word
+
+
+    return blob, cursor
+
+def parse_type_compound_helper(fcontent, cursor) -> tuple[str, int]:
+    next, cursor = next_token_unwrap(fcontent, cursor)
+    if next == "{":
+        compound, cursor = parse_compound_type(fcontent, cursor)
+        return "{\n" + '\n'.join(f'\t{line}' for line in compound.splitlines()) + "\n}", cursor
+
+    ## assuming next is a 'word'
+    name = next
+    next, next_cursor = next_token_unwrap(fcontent, cursor)
+    if next == "{":
+        compound, cursor = parse_compound_type(fcontent, cursor)
+        return "{\n" + '\n'.join(f'\t{line}' for line in compound.splitlines()) + "\n}", cursor
+    elif next == "*":
+        return "^" + name, next_cursor
+
+    return name, cursor
+
+
+# <type> ::= { "struct" | "union" } [word] [<body>] | <word_seq> [ "*"* ]
+# type
+#    : base_type
+#    | type '*'
+#    | type '[' constant_expr ']'
+#    | type '[' ']'
+#    | 'const' type
+#    ;
+def parse_type(fcontent, cursor) -> tuple[Type, int]:
+    base: str | None = None
+    sign_idx = 0  # signed == 0; unsigned == 1
+    base, cursor = next_token_unwrap(fcontent, cursor)
+    # ignore const...
+    if base == "const":
+        base, cursor = next_token_unwrap(fcontent, cursor)
+    elif base == "signed":
+        base, cursor = next_token_unwrap(fcontent, cursor)
+    elif base == "unsigned":
+        base, cursor = next_token_unwrap(fcontent, cursor)
+        sign_idx = 1
+
+    print(f"\x1b[31mParsing base:\x1b[0m {base}")
+    print(f"\x1b[31mParsing base (next):\x1b[0m {next_token_unwrap(fcontent, cursor)}")
+
+    if base in c_to_odin_types:
+        # clGetPlatformIDs_t * -> ^clGetPlatformIDs_t
+
+        potential_ptr, potential_cursor = next_token_unwrap(fcontent, cursor)
+        if potential_ptr == "*":
+            return "^" + c_to_odin_types[base][sign_idx], potential_cursor
+        return c_to_odin_types[base][sign_idx], cursor
+
+    for alias in g_aliases:
+        #print(f"{alias}: {g_aliases[alias]}")
+        pass
+
+    if base in g_aliases:
+        potential_ptr, potential_cursor = next_token_unwrap(fcontent, cursor)
+        if potential_ptr == "*":
+            return "^" + g_aliases[base], potential_cursor
+        return g_aliases[base], cursor
+    elif base in g_aliases.values():
+        potential_ptr, potential_cursor = next_token_unwrap(fcontent, cursor)
+        if potential_ptr == "*":
+            return "^" + base, potential_cursor
+        return base, cursor
+
+
+    if base == "struct" or base == "union":
+        # struct{ cl_char x, y; }; -> using _###: struct { x, y: cl_char; }
+
+        # struct _cl_sampler * -> ^_cl_sampler (additional filter needed later: -> distinct rawptr)
+
+        # struct _cl_image_format {
+        #   cl_channel_order image_channel_order;
+        #   cl_channel_type image_channel_data_type;
+        # }
+        # => _cl_image_format :: struct {
+        #   image_channel_order: cl_channel_order;
+        #   image_channel_data_type: cl_channel_type;
+        # }
+
+        blob, cursor = parse_type_compound_helper(fcontent, cursor)
+        if cursor != ";":
+            _, cursor = next_token_unwrap(fcontent, cursor)
+
+        if base == "union":
+            return "struct #raw_union " + blob, cursor
+
+        return base + blob, cursor
+
+    assert False
+
+def parse_conv(fcontent, cursor) -> tuple[Attribute, int]:
+    word, next_cursor = next_token_unwrap(fcontent, cursor)
+
+    if word == "__stdcall":
+        return Attribute.STDCALL, next_cursor
+    elif word == "__attribute__":
+        for i in range(0, 2):
+            word, next_cursor = next_token_unwrap(fcontent, next_cursor)
+            assert word == "("
+
+        word, next_cursor = next_token_unwrap(fcontent, next_cursor)
+        assert word == "__stdcall__"
+
+        for i in range(0, 2):
+            word, next_cursor = next_token_unwrap(fcontent, next_cursor)
+            assert word == ")"
+
+        return Attribute.STDCALL, next_cursor
+
+    return Attribute.NONE, cursor
+
+def parse_params(fcontent, cursor) -> tuple[list[tuple[Type, str]], int]:
+    params: list[tuple[Type, str]] = []
+
+    word, next_cursor = next_token_unwrap(fcontent, cursor)
+    assert word == "("
+
+    while True:
+        word, next_cursor = next_token_unwrap(fcontent, next_cursor)
+        if word == ")":
+            break
+
+        _type = word
+        name, next_cursor = next_token_unwrap(fcontent, next_cursor)
+        params.append((_type, name))
+
+        word, next_cursor = next_token_unwrap(fcontent, next_cursor)
+        if word == ")":
+            break
+        elif word != ",":
+            raise Exception(f"Unexpected token in param list: {word}")
+
+    return params, next_cursor
+
+def parse(fcontent: str) -> None:
+    cursor = 0
+
+    while True:
+        result = next_token(fcontent, cursor)
+        if not result:
+            break
+
+        word, next_cursor = result
+
+        match word:
+            case "extern":
+                # <fn_decl>     ::= "extern" <return_type> [<convention>] [<attribute>] <fname> ([<params>]);
+                # <return_type> ::= <type>
+                # <type>        ::= { "struct" | "union" } [word] [<body>] | <word_seq> [ "*"* ]
+                # <word_seq>    ::= (word)+
+                # <params>      ::= <param> | <param> <params>
+                # <param>       ::= <type> word
+                # <convention>  ::= __stdcall
+                # <attribute>   ::= __attribute__((__stdcall__))
+                # <fname>       ::= word
+                f = Function()
+
+                print("Parsing Function: ")
+                f.ret, next_cursor    = parse_type(fcontent, next_cursor)
+                print(f"\tFound return type: {f.ret}")
+                f.attr, next_cursor   = parse_conv(fcontent, next_cursor) # <convention> == <attribute>
+                print(f"\tFound attribute: {"none" if f.attr == Attribute.NONE else "stdcall"}")
+                f.name, next_cursor   = next_token_unwrap(fcontent, next_cursor)
+                print(f"\tFound name: {f.name}")
+                f.params, next_cursor = parse_params(fcontent, next_cursor)
+                print(f"\tFound params: {' '.join(f'{param[0]}{param[1]}' for param in f.params)}")
+                print(f"Result: `{f.into_odin()}`")
+
+                word, next_cursor     = next_token(fcontent, next_cursor)
+                assert word == ";"
+
+            case "typedef":
+                # <typedef> ::= "typedef" <type> word ";"
+                a = Alias()
+
+                a._from, next_cursor = parse_type(fcontent, next_cursor)
+                a._to, next_cursor = next_token_unwrap(fcontent, next_cursor)
+                print(f"Found alias: <{a._from} | {a._to}>")
+
+                terminator, next_cursor = next_token_unwrap(fcontent, next_cursor)
+                #print(f"Terminator: `{terminator}` {len(terminator)}\nnext_cursor: {next_cursor} vs. len: {len(fcontent)}")
+                assert terminator == ";"
+
+                g_aliases[a._to] = a._from
+
+            case "#define":
+                # <define> ::= "#define" word word
+
+                d = Definition()
+                d.key, next_cursor   = next_token_unwrap(fcontent, next_cursor)
+                d.value, potential_cursor = next_token_unwrap(fcontent, next_cursor)
+                # note: this will miss some of the #define(s) but they are not important to the bindings
+                match d.value:
+                    case "#define" | "typedef" | "extern":
+                        # blank define, useless for us
+                        pass
+                    case _:
+                        next_cursor = potential_cursor
+                        g_definitions.append(d)
+
+            case _:
+                print(f"\x1b[31mSkipping token\x1b[0m: \x1b[33m{word}\x1b[0m")
+
+        cursor = next_cursor
+
+"""
+Determines whether given C type is opaque (i.e. in Odin `distinct rawptr')
+"""
+def is_opaque(value: str) -> bool:
+    opaque_pattern = re.compile(r"^struct\s+\w+\s*\*$")
+    return opaque_pattern.match(value) is not None
+
+def preprocess_run(cc: str, current_location: str, target: str) -> str:
+    include_path   = os.path.join(current_location, "../")
+    preprocess_out = os.path.join(current_location, f"{target}.out.txt")
+    target         = os.path.join(current_location, target)
+    subprocess.run(f"{cc} -dD -E \"{target}\" -I \"{include_path}\" -o \"{preprocess_out}\"", check=True)
+    return preprocess_out
+
+def main(cc: str, out_dir: str) -> None:
+    print("parsegen turned \x1b[32mON\x1b[0m")
+    print(f"Running parsegen → Output: {out_dir}")
+
+    #header_files = ["cl_platform.h", "cl.h", "cl_version.h", "cl_icd.h"]
+    header_files = ["cl_platform.h"]
+    current_location = os.path.dirname(os.path.abspath(__file__))
+
+    for target_header in header_files:
+        preprocess_fout = preprocess_run(cc, current_location, target_header)
+        preprocess_extract = extract_file_content(preprocess_fout, target_header)
+        with open(f"{preprocess_fout}.ex", "w+") as file:
+            file.write(preprocess_extract)
+        parse(preprocess_extract)
+
+    file_blob: str = ""
+
+    for defs in g_definitions:
+        file_blob += defs.into_odin() + '\n'
+
+    for alias in g_aliases:
+        file_blob += alias + " :: " + g_aliases[alias] + '\n'
+
+    for func in g_functions:
+        file_blob += func + '\n'
+
+    # aliases_str: str = ""
+    # aliases_rec: set[str] = set()
+    # for value in g_aliases:
+    #     if g_aliases[value] not in aliases_rec:
+    #         ctype_idx = 0
+    #         if "unsigned" in c_to_odin_types:
+    #             ctype_idx = 1
+    #
+    #         if is_opaque(value):
+    #             aliases_str += f"{g_aliases[value]} :: distinct rawptr; // {value}\n";
+    #         else:
+    #             if value in c_to_odin_types:
+    #                 aliases_str += f"{g_aliases[value]} :: {c_to_odin_types[value][ctype_idx]};\n"
+    #             else:
+    #                 aliases_str += f"{g_aliases[value]} :: {value};\n"
+    #
+    #         aliases_rec.add(g_aliases[value])
+
+    with open(os.path.join(out_dir, "out.odin"), "w+") as file:
+        file.write("package cl;\n\n")
+        # file.write(aliases_str)
+        file.write(file_blob)
